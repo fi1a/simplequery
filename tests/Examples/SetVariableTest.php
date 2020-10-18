f209dce1e42f501af97c6e88318214934ccfaea5
Nikolay Gorohovsky <fi1a@icloud.com>
2020-10-18 14:57:39 +0500

    SetVariableTest

---
+++ tests/Examples/SetVariableTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fi1a\Unit\SimpleQuery\Examples;
+
+use Fi1a\SimpleQuery\SimpleQuery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Устанавливает переменную.
+ */
+class SetVariableTest extends TestCase
+{
+    /**
+     * Установить переменные с ключами "news" и "title".
+     */
+    public function testSetVariable(): void
+    {
+        $html = file_get_contents(__DIR__ . '/Fixtures/SetVariable.html');
+        $sq = new SimpleQuery($html);
+        $sq->setVariable('news', 'body > .content > .news');
+        $sq->setVariable('title', 'h1');
+        $this->assertEquals('News title', $sq('{{news}} > {{title}}')->text());
+    }
+}
