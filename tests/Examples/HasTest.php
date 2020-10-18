56344c87ec1f83f76ef4651ebe424ba57b191c36
Nikolay Gorohovsky <fi1a@icloud.com>
2020-10-18 15:28:32 +0500

    HasTest

---
+++ tests/Examples/HasTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fi1a\Unit\SimpleQuery\Examples;
+
+use Fi1a\SimpleQuery\SimpleQuery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Есть ли элемент с таким ключем.
+ */
+class HasTest extends TestCase
+{
+    /**
+     * Есть ли элемент с таким ключем.
+     */
+    public function testHas(): void
+    {
+        $html = file_get_contents(__DIR__ . '/Fixtures/Has.html');
+        $sq = new SimpleQuery($html);
+        $this->assertTrue($sq('div')->has(0));
+    }
+}
