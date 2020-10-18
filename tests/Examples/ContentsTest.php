e5a57187c96f3e9abc78b308a3a4933b2ff9efc6
Nikolay Gorohovsky <fi1a@icloud.com>
2020-10-18 09:13:46 +0500

    ContentsTest

---
+++ tests/Examples/ContentsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fi1a\Unit\SimpleQuery\Examples;
+
+use Fi1a\SimpleQuery\SimpleQuery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Возвращает все дочерние элементы в том числе и текст.
+ */
+class ContentsTest extends TestCase
+{
+    /**
+     * Обернуть все содержимое тега "p" тегами "b".
+     */
+    public function testContents(): void
+    {
+        $html = file_get_contents(__DIR__ . '/Fixtures/Contents.html');
+        $result = file_get_contents(__DIR__ . '/Fixtures/Contents-result.html');
+        $sq = new SimpleQuery($html);
+        $sq('p')->contents()->wrap('<b></b>');
+        $this->assertEquals($result, (string) $sq);
+    }
+}
