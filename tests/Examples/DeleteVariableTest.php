e50aed0031c5617898213011626dd14cb1ba4996
Nikolay Gorohovsky <fi1a@icloud.com>
2020-10-18 15:19:51 +0500

    DeleteVariableTest

---
+++ tests/Examples/DeleteVariableTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fi1a\Unit\SimpleQuery\Examples;
+
+use Fi1a\Collection\Collection;
+use Fi1a\SimpleQuery\SimpleQuery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Удаляет переменную.
+ */
+class DeleteVariableTest extends TestCase
+{
+    /**
+     * Удаляет переменную.
+     */
+    public function testDeleteVariable(): void
+    {
+        $sq = new SimpleQuery();
+        $variables = new Collection([
+            'news' => 'body > .content > .news',
+            'title' => 'h1',
+        ]);
+        $sq->setVariables($variables);
+
+        $this->assertTrue($sq->hasVariable('news'));
+        $sq->deleteVariable('news');
+        $this->assertFalse($sq->hasVariable('news'));
+    }
+}
