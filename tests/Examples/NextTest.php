4e0e36f54a7e2932e746d88c22baedf415d591e1
Nikolay Gorohovsky <fi1a@icloud.com>
2020-10-18 10:31:47 +0500

    NextTest

---
+++ tests/Examples/NextTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fi1a\Unit\SimpleQuery\Examples;
+
+use Fi1a\SimpleQuery\SimpleQuery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Получить следующий элемент. Если предоставлен селектор, он извлекает следующий элемент только в том случае,
+ * если он соответствует этому селектору.
+ */
+class NextTest extends TestCase
+{
+    /**
+     * Найти следующий элемент каждой не активной кнопки.
+     */
+    public function testNext(): void
+    {
+        $html = file_get_contents(__DIR__ . '/Fixtures/Next.html');
+        $result = file_get_contents(__DIR__ . '/Fixtures/Next-result.html');
+        $sq = new SimpleQuery($html);
+        $sq('button[disabled]')->next()->text('this button is disabled');
+        $this->assertEquals($result, (string) $sq);
+    }
+}
