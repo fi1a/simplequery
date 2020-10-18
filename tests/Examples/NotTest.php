3927ef43c923e202602e06fb9aaa1d5b26a7cadc
Nikolay Gorohovsky <fi1a@icloud.com>
2020-10-18 11:10:57 +0500

    NotTest

---
+++ tests/Examples/NotTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fi1a\Unit\SimpleQuery\Examples;
+
+use Fi1a\SimpleQuery\SimpleQuery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Убрать элементы соответствующие селектору.
+ */
+class NotTest extends TestCase
+{
+    /**
+     * Добавить border к div'ам не имеющим класс "green" и не имеющим идентификатор "blueone".
+     */
+    public function testNot(): void
+    {
+        $html = file_get_contents(__DIR__ . '/Fixtures/Not.html');
+        $result = file_get_contents(__DIR__ . '/Fixtures/Not-result.html');
+        $sq = new SimpleQuery($html);
+        $sq('div')->not('.green, #blueone')->css('border-color', 'red');
+        $this->assertEquals($result, (string) $sq);
+    }
+}
