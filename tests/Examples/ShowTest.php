d243ce02c28f8248a37ca9b2203bcd0e9cf9308f
Nikolay Gorohovsky <fi1a@icloud.com>
2022-10-01 07:44:10 +0500

    Show example test

---
+++ tests/Examples/ShowTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fi1a\Unit\SimpleQuery\Examples;
+
+use Fi1a\SimpleQuery\SimpleQuery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Отобразить элементы.
+ */
+class ShowTest extends TestCase
+{
+    /**
+     * Отобразить скрытый параграф.
+     */
+    public function testEmptySelector(): void
+    {
+        $html = file_get_contents(__DIR__ . '/Fixtures/Show.html');
+        $result = file_get_contents(__DIR__ . '/Fixtures/Show-result.html');
+        $sq = new SimpleQuery($html);
+        $sq('p')->show();
+        $this->assertEquals($result, (string) $sq);
+    }
+}
